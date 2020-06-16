b7dc2e699daed39ccffc25c75dbd896aaa5cfaca
smallljl <168241296@qq.com>
2020-06-17 00:21:16 +0800

    a

---
+++ Week_09/16-680. 验证回文字符串 Ⅱ.js
@@ -0,0 +1,22 @@
+/**
+ *  给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
+
+    示例 1:
+
+    输入: "aba"
+    输出: True
+    示例 2:
+
+    输入: "abca"
+    输出: True
+    解释: 你可以删除c字符。
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/valid-palindrome-ii
+ *  
+ * 
+ * 
+ */
+var validPalindrome = function(s) {
+
+};
