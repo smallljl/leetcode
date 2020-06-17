e016040137fe9576f8b357b6ef60c379a784fc9f
smallljl <168241296@qq.com>
2020-06-17 09:47:40 +0800

    a

--- Week_09/01-709. 转换成小写字母.js
+++ Week_09/01-709. 转换成小写字母.js
@@ -20,5 +20,13 @@
  * 
  */
 var toLowerCase = function(str) {
-
+   let res = "";
+   for(let i = 0; i < str.length;i++){
+      let code = str.charCodeAt(i);
+      if(code <= 90 && code >=65)
+         res += String.fromCharCode(code+32);
+      else 
+         res += str[i];
+   }
+   return res;
 };
