acf2d4df347bc16e1d4a5f615c232d2ead3aa737
smallljl <168241296@qq.com>
2020-06-17 16:42:35 +0800

    宝石

--- Week_09/01-709. 转换成小写字母.js
+++ Week_09/01-709. 转换成小写字母.js
@@ -23,10 +23,8 @@ var toLowerCase = function(str) {
    let res = "";
    for(let i = 0; i < str.length;i++){
       let code = str.charCodeAt(i);
-      if(code <= 90 && code >=65)
-         res += String.fromCharCode(code+32);
-      else 
-         res += str[i];
+      res += (code <= 90 && code >=65) ?
+          String.fromCharCode(code+32) : str[i];
    }
    return res;
 };
