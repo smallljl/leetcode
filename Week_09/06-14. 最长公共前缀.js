7ce2abc9b7cf261f4a1a28300e7d23261df8c410
smallljl <168241296@qq.com>
2020-06-19 18:26:36 +0800

    最长公共前缀

--- Week_09/06-14. 最长公共前缀.js
+++ Week_09/06-14. 最长公共前缀.js
@@ -20,5 +20,17 @@
  * 
  */
 var longestCommonPrefix = function(strs) {
-    
+   if(strs.length == 0) return "";
+   let ans = strs[0];
+   for(let i =1;i<strs.length;i++) {
+      let j=0;
+      while(j < ans.length && j < strs[i].length){
+         if(ans[j] != strs[i][j])
+            break;
+         j++;
+      }
+      ans = ans.substr(0, j);  // 截取
+      if(ans === "") return ans;
+   }
+   return ans;
 };
