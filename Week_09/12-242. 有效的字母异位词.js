b5350ccea784ad8ec9ca30e3ad2f5d67e17e4cb0
smallljl <168241296@qq.com>
2020-06-24 15:01:18 +0800

    最长回文子串

--- Week_09/12-242. 有效的字母异位词.js
+++ Week_09/12-242. 有效的字母异位词.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-17 09:22:51
+ * @LastEditTime: 2020-06-24 11:19:26
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \leetcode\Week_09\12-242. 有效的字母异位词.js
+ */ 
 /**
  *  给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
 
@@ -17,5 +25,25 @@
  * 
  */
 var isAnagram = function(s, t) {
-    
+   if (s === "" && t === "") return true
+   if (s.length > t.length) return false
+   let hashMap = new Map()
+   for(let i = 0; i < s.length; i++) {
+       if (!hashMap.has(s[i])) {
+           hashMap.set(s[i], 1)
+       } else {
+           hashMap.set(s[i], hashMap.get(s[i])+1)
+       }
+   }
+   for(let i = 0; i < t.length; i++) {
+       if(!hashMap.has(t[i])){
+        return false
+     }
+       if(hashMap.get(t[i]) > 0){
+           hashMap.set(t[i], hashMap.get(t[i])-1)
+       } else {
+           return false
+       }
+   }
+   return true
 };
--- Week_09/17-5. 最长回文子串.js
+++ Week_09/17-5. 最长回文子串.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-17 09:22:51
+ * @LastEditTime: 2020-06-24 15:00:22
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \leetcode\Week_09\17-5. 最长回文子串.js
+ */ 
 /**
  *  给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
 
@@ -17,6 +25,29 @@
  * 
  * 
  */
-var longestPalindrome = function(s) {
+var isPalindrome = function(charArray,left,right) {
+   while(left < right){
+      if(charArray[left] !== charArray[right]) return false;
+      left++;
+      right--;
+   }
+   return true;
+};
 
+
+var longestPalindrome = function(s) {
+   let len = s.length;
+   if(len < 2) return s;
+   let maxLen = 1;
+   let begin = 0;
+   let charArray = s.split("");
+   for(let i = 0; i < len - 1; i++){
+      for(let j = i + 1; j < len; j++){
+         if(j - i + 1 > maxLen && isPalindrome(charArray,i,j)){
+            maxLen = j - i + 1;
+            begin = i;
+         }
+      }
+   }
+   return s.substr(begin,maxLen);
 };
