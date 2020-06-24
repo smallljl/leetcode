db538bda3a63dd9cc0094c2cc8d51cd2b9ecab3a
smallljl <168241296@qq.com>
2020-06-24 09:15:58 +0800

    反转字符传

--- Week_09/08-541. 反转字符串 II.js
+++ Week_09/08-541. 反转字符串 II.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-17 09:22:51
+ * @LastEditTime: 2020-06-24 09:15:40
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \leetcode\Week_09\08-541. 反转字符串 II.js
+ */ 
 /**
  *  给定一个字符串 s 和一个整数 k，你需要对从字符串开头算起的每隔 2k 个字符的前 k 个字符进行反转。
 
@@ -17,5 +25,16 @@
  * 
  */
 var reverseStr = function(s, k) {
-
+   s = s.split("");
+   for(let i = 0,len = s.length; i < len; i += 2*k ){
+      let left = i,
+          right = Math.min(left+k-1,len-1);
+          debugger
+      while (left < right){
+          let temp = s[left];
+          s[left++] = s[right];
+          s[right--] = temp;
+      }
+  }
+  return s.join("");
 };
