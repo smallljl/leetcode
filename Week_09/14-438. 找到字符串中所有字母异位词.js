16d1a5920c1b6ca3c0ee3c5b6ae8f82aa62598b8
smallljl <168241296@qq.com>
2020-06-29 18:37:15 +0800

    找字符串中的异位词

--- Week_09/14-438. 找到字符串中所有字母异位词.js
+++ Week_09/14-438. 找到字符串中所有字母异位词.js
@@ -37,6 +37,45 @@
  * 
  * 
  */
-var findAnagrams = function(s, p) {
+let s = "cbaebabacd", p="abc";
 
+var isAnagram = function(s, t) {
+   if (s === "" && t === "") return true
+   if (s.length > t.length) return false
+   let hashMap = new Map()
+   for(let i = 0; i < s.length; i++) {
+       if (!hashMap.has(s[i])) {
+           hashMap.set(s[i], 1)
+       } else {
+           hashMap.set(s[i], hashMap.get(s[i])+1)
+       }
+   }
+   for(let i = 0; i < t.length; i++) {
+       if(!hashMap.has(t[i])){
+        return false
+     }
+       if(hashMap.get(t[i]) > 0){
+           hashMap.set(t[i], hashMap.get(t[i])-1)
+       } else {
+           return false
+       }
+   }
+   return true
 };
+
+var findAnagrams = function(s, p) {
+       let ans = [];
+       for(let i = 0; i < s.length;i++){
+          let flag = true;
+          for(let j = 0; j < p.length;j++){
+             if(s[i] !== s[j]){
+                flag = false;
+             }
+          }
+          if(flag){
+              ans.push(i);
+          }
+       }
+       return ans;
+};
+console.log(findAnagrams(s,p));
