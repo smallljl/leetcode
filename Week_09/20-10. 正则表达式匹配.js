f32e0017cbde6272fefa46d631e22445567993ce
smallljl <168241296@qq.com>
2020-07-02 21:50:08 +0800

    1

--- Week_09/20-10. 正则表达式匹配.js
+++ Week_09/20-10. 正则表达式匹配.js
@@ -51,5 +51,14 @@
  * 
  */
 var isMatch = function(s, p) {
-
+   // 边界情况，如果s和p都为空，说明处理结束了，返回true，否则返回false
+   if (p.length <= 0) 
+      return !s.length;
+   // 判断p模式字符串的第一个字符和s字符串的第一个字符是不是匹配
+   let match = false;
+   (s.length > 0 && (p[0] === s[0] || p[0] === '.')) &&  (match = true);
+   // p有模式的  判断后面是否大于1  大于1就有模式
+   return (p.length > 1 && p[1] === '*') ? 
+      (isMatch(s, p.slice(2)) || (match && isMatch(s.slice(1), p))) :
+      (match && isMatch(s.slice(1), p.slice(1)));
 };
