ee7852774836082f109bfb940e6f52278b57e29f
smallljl <168241296@qq.com>
2020-07-10 17:19:23 +0800

    a

---
+++ Week_08/10-493. 翻转对.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-06 16:06:09
+ * @LastEditTime: 2020-07-06 16:06:36
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \leetcode\Week_08\10-493. 翻转对.js
+ */ 
+/*
+  给定一个数组 nums ，如果 i < j 且 nums[i] > 2*nums[j] 我们就将 (i, j) 称作一个重要翻转对。
+
+  你需要返回给定数组中的重要翻转对的数量。
+
+  示例 1:
+
+  输入: [1,3,2,3,1]
+  输出: 2
+  示例 2:
+
+  输入: [2,4,3,5,1]
+  输出: 3
+
+  来源：力扣（LeetCode）
+  链接：https://leetcode-cn.com/problems/reverse-pairs
+ */ 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var reversePairs = function(nums) {
+
+};
