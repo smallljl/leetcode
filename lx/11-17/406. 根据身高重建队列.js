16f991a75dcd0a780c1ab0304c325255b32bfe55
lijiaolong <168241296@qq.com>
2020-11-18 10:14:47 +0800

    a

--- lx/11-17/406. 根据身高重建队列.js
+++ lx/11-17/406. 根据身高重建队列.js
@@ -18,5 +18,10 @@
  */
 
 var reconstructQueue = function(people) {
-
+    people.sort((a,b) => a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]);
+    let ans = [];
+    for(let i = 0; i < people.length; i++){
+        ans.splice(people[i][1],0,people[i]);
+    };
+    return  ans;
 };
