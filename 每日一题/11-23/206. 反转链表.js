fb20331d3f753d973e6faaabfccb5dea584de225
lijiaolong <168241296@qq.com>
2020-11-23 19:44:50 +0800

    反转链表

--- 每日一题/11-23/206. 反转链表.js
+++ 每日一题/11-23/206. 反转链表.js
@@ -25,3 +25,16 @@ var reverseList = function(head) {
     }
     return prev;
 };
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(!head || !head.next) return head;
+    let next = head.next; // next节点，反转后是最后一个节点
+    let reverseHead = reverseList(next);
+    head.next = null;  // 裁减 head
+    next.next = head;  // 尾接
+    return reverseHead
+};
