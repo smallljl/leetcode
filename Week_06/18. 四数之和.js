ee76f09cf52c9f0e80470e4ad91dc6e06419e811
lijiaolong <168241296@qq.com>
2020-11-23 10:50:48 +0800

    a

--- Week_06/18. 四数之和.js
+++ Week_06/18. 四数之和.js
@@ -49,31 +49,31 @@ let gen = (nums,target)=>{
    if(nums === null || len < 4) return [];
    nums.sort((a,b)=>a-b);
    for(let i = 0; i < len - 3;i++){
-         if(i>0 && nums[i] === nums[i-1]) continue;
-         let min1 = nums[i] + nums[i+1] + nums[i+2] + nums[i+3];
-         if(min1 > target) break;
-         let max1 = nums[i] + nums[len-1] + nums[len-2] + nums[len-3];
-         if(max1 < target) continue;
-         for(let j = i+1; j < len - 2;j++){
-            if(j > i+1 && nums[j] === nums[j-1]) continue;
-            let min2 = nums[i] + nums[j] + nums[j+1] + nums[j+2];
-            if(min2 > target) break;
-            let max2 = nums[i] + nums[j] + nums[len-1] + nums[len-2];
-            if(max2 < target) continue;
-            let left = j+1;
-            let right = len-1;
-            while(left < right){
-               let sum = nums[i] + nums[j] + nums[left] + nums[right];
-               if(sum === target){
-                     result.push([ nums[i],nums[j],nums[left],nums[right]])
-                     while(left < right && nums[left] === nums[left + 1]) left++;
-                     while(left < right && nums[right] === nums[right - 1]) right--;
-                     left ++;
-                     right --;
-               } else 
-                     sum > target ? right-- : left++;
-            }
+      if(i>0 && nums[i] === nums[i-1]) continue;
+      let min1 = nums[i] + nums[i+1] + nums[i+2] + nums[i+3];
+      if(min1 > target) break;
+      let max1 = nums[i] + nums[len-1] + nums[len-2] + nums[len-3];
+      if(max1 < target) continue;
+      for(let j = i+1; j < len - 2;j++){
+         if(j > i+1 && nums[j] === nums[j-1]) continue;
+         let min2 = nums[i] + nums[j] + nums[j+1] + nums[j+2];
+         if(min2 > target) break;
+         let max2 = nums[i] + nums[j] + nums[len-1] + nums[len-2];
+         if(max2 < target) continue;
+         let left = j+1;
+         let right = len-1;
+         while(left < right){
+            let sum = nums[i] + nums[j] + nums[left] + nums[right];
+            if(sum === target){
+                  result.push([ nums[i],nums[j],nums[left],nums[right]])
+                  while(left < right && nums[left] === nums[left + 1]) left++;
+                  while(left < right && nums[right] === nums[right - 1]) right--;
+                  left ++;
+                  right --;
+            } else 
+                  sum > target ? right-- : left++;
          }
+      }
    }
    return result;
 }
