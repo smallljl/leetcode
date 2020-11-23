09eafd389bfa092293e6a47f0d1f8704be1e59e3
lijiaolong <168241296@qq.com>
2020-11-23 17:21:23 +0800

    反转链表 加油鸭

---
+++ 每日一题/11-23/206. 反转链表.js
@@ -0,0 +1,27 @@
+/**
+ *  反转一个单链表。
+
+    示例:
+
+    输入: 1->2->3->4->5->NULL
+    输出: 5->4->3->2->1->NULL
+    进阶:
+    你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+
+ * 
+ * 
+ */
+
+var reverseList = function(head) {
+    let prev = null;
+    let curr =  head;
+    while(curr !== null){
+        let nextTemp = curr.next;  // 保存下一个节点的值
+        curr.next = prev;
+        
+        // 后移动
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+};
