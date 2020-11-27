6b76980466e808cd0bc94e618de2338a0c4fed6e
lijiaolong <168241296@qq.com>
2020-11-27 09:46:47 +0800

    四数求和

--- 每日一题/2020-11/11-27/454. 四数相加 II.js
+++ 每日一题/2020-11/11-27/454. 四数相加 II.js
@@ -23,3 +23,26 @@
 
  * 
  */
+
+var fourSumCount = function(A, B, C, D) {
+    if( 0 === A.length * B.length * C.length * D.length) return 0;
+    const map = new Map();
+    for(let i = 0; i < C.length; i++){
+        for(let j = 0; j < D.length; j++){
+            const sum = C[i] + D[j];
+            if(!map.has(sum)){
+                map.set(sum,1);
+            } else {
+                map.set(sum, map.get(sum) + 1);
+            }
+        }
+    }
+    let count = 0;
+    for(let i = 0; i < A.length; i++){
+        for(let j = 0; j < B.length; j++){
+            const sum = A[i] + B[j];
+            map.has(-sum) && (count += map.get(-sum));
+        }
+    }
+    return count;
+};
