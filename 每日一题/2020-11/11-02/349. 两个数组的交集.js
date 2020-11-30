e6883801fb82eea482814dbf7080706303abcda8
lijiaolong <168241296@qq.com>
2020-11-30 11:53:24 +0800

    test

--- 每日一题/2020-11/11-02/349. 两个数组的交集.js
+++ 每日一题/2020-11/11-02/349. 两个数组的交集.js
@@ -15,7 +15,21 @@
  * 
  * 
  */
-
-var intersection = function(nums1, nums2) {
-
-};
+var _intersection = function(set1,set2){
+    if(set1.size > set2.size){
+        _intersection(set2,set1);
+    }
+    const intersection = new Set();
+    for(let item of set2){
+        if(set1.has(item)){
+           intersection.add(item);
+        }
+    }
+    return [...intersection];
+  }
+  
+  var intersection = function(nums1, nums2) {
+     const set1 = new Set(nums1);
+     const set2 = new Set(nums2);
+     return _intersection(set1, set2);
+  };
