5ca4af736dd3bb83d46a9f05bcbf912f6b6e40ca
lijiaolong <168241296@qq.com>
2020-12-18 17:44:59 +0800

    计数

--- 每日一题/2020-12/12-18/389. 找不同.js
+++ 每日一题/2020-12/12-18/389. 找不同.js
@@ -47,3 +47,20 @@ var findTheDifference = function(s, t) {
     }
     return String.fromCharCode(at - as);
 };
+
+/**
+ * 思路:计数
+ */
+var findTheDifference = function(s, t) {
+    const cnt = new Array(26).fill(0);
+    for(const ch of s){
+        cnt[ch.charCodeAt() - "a".charCodeAt()]++;
+    }
+    for(const ch of t){
+        cnt[ch.charCodeAt() - "a".charCodeAt()]--;
+        if(cnt[ch.charCodeAt() - "a".charCodeAt()] < 0){
+            return ch;
+        }
+    }
+    return " ";
+};
