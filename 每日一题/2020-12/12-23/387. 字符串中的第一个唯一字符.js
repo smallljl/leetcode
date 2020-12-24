b0ced586793dd17e5f7c50c187e180d11384ddf0
lijiaolong <168241296@qq.com>
2020-12-24 09:16:43 +0800

    387

--- 每日一题/2020-12/12-23/387. 字符串中的第一个唯一字符.js
+++ 每日一题/2020-12/12-23/387. 字符串中的第一个唯一字符.js
@@ -34,3 +34,27 @@ var firstUniqChar = function(s) {
     }
     return -1;
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// 思路值为-1就弹出去
+var firstUniqChar = function(s) {
+    const position = new Map();
+    const q = [];
+    const n = s.length;
+    for(let [i, ch] of Array.from(s).entries()){
+        if(!position.has(ch)){
+            position.set(ch,i);
+            q.push([s[i],i]);
+        } else {
+            position.set(ch, -1);
+            // 如果第1个字符有重复就出队列
+            while(q.length && position.get(q[0][0]) === -1){
+                q.shift();
+            }
+        }
+    }
+    return q.length ? q[0][1]: -1;
+};
