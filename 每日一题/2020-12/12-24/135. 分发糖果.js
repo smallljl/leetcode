bdd60fc71c029c255cc3b3595eb8d27b52cd735f
lijiaolong <168241296@qq.com>
2020-12-24 17:01:00 +0800

    135

--- 每日一题/2020-12/12-24/135. 分发糖果.js
+++ 每日一题/2020-12/12-24/135. 分发糖果.js
@@ -51,3 +51,31 @@ var candy = function(ratings) {
     }
     return ans;
 };
+
+// 不理解
+var candy = function(ratings) {
+    const n = ratings.length;
+    let ret = 1;
+    // dec 当前递减序列的长度 
+    // inc 最近的递增序列的长度
+    // pre 前一个同学分得的糖果数量
+    let inc = 1, dec = 0, pre = 1;   
+
+    for (let i = 1; i < n; i++) {
+        if (ratings[i] >= ratings[i - 1]) {
+            dec = 0;
+            if (ratings[i] === ratings[i - 1]) pre = 1;
+            else pre++;
+            ret += pre;
+            inc = pre;
+        } else {
+            dec++;
+            if (dec === inc) {
+                dec++;
+            }
+            ret += dec;
+            pre = 1;
+        }
+    }
+    return ret;
+};
