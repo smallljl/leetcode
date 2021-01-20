b7024a9316ffd5da6429dd1e067282200bb42514
lijiaolong <168241296@qq.com>
2021-01-20 11:01:26 +0800

    76

--- Week_06/76. 最小覆盖子串.js
+++ Week_06/76. 最小覆盖子串.js
@@ -20,7 +20,45 @@
  * 
  * 
  * 
+ */
+
+/**
+ * 滑动窗口解法
+ * @param {*} s 
+ * @param {*} t 
  */ 
 var minWindow = function(s, t) {
-
+   let left = 0,right = 0;
+   let needs = {}; // 需要匹配
+   let windows = {};  // 滑动的窗口
+   let match = 0, start = 0, minLen = Number.MAX_SAFE_INTEGER;
+   for(let i = 0; i < t.length; i++){
+       needs[t[i]] ? needs[t[i]]++ : needs[t[i]] = 1;
+   }
+   let needsLen = Object.keys(needs).length;  // 匹配的类型的数量
+   while(right < s.length){
+       let c1 = s[right];
+       if(needs[c1]){
+           windows[c1] ? windows[c1]++ : windows[c1] = 1;
+           if(windows[c1] === needs[c1]){
+               match++;
+           }
+       }
+       right++;
+       while(match === needsLen){
+           if(right - left < minLen){
+               start = left;
+               minLen = right - left;
+           }
+           let c2 = s[left];
+           if(needs[c2]){
+               windows[c2]--;
+               if(windows[c2] < needs[c2]){
+                   match--;
+               }
+           }
+           left++;
+       }
+   }
+   return minLen === Number.MAX_SAFE_INTEGER ? "" : s.substr(start,minLen);
 };
